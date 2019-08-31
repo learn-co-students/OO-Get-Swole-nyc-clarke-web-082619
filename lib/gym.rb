17abae357db5dcbfbeb3ad54f1b07dd2dabab981
JazzyMussels <jeffmussels@gmail.com>
2019-08-31 12:39:16 -0400

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,30 @@
 class Gym
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def self.all 
+    @@all 
+  end
+
+  def self.find_by_name(gym_name)
+    self.all.select{|gym| gym.name == gym_name}
+  end
+
+  def memberships
+    Membership.all.select{|member| member.gym == self}
+  end
+
+  def lifter_names
+    memberships.map{|member| member.lifter}
+  end
+
+  def combined_lift_total
+    lifter_names.map{|member| member.lift_total}.reduce(&:+)
+  end
+
 end
