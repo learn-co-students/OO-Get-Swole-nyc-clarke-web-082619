cef2dfa803717f80aaaa29cb577f0c303c1e1ece
github username <github email address>
2019-09-03 13:11:37 -0400

    Done

--- lib/gym.rb
+++ lib/gym.rb
@@ -4,4 +4,26 @@ class Gym
   def initialize(name)
     @name = name
   end
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select {|m|m.gym == self}
+  end
+
+  def lifters
+    memberships.map {|m| m.lifter}
+  end
+
+  def lifters_names
+    lifters.map {|l| l.name}
+  end
+
+  def lifters_lift_total
+    lifters.reduce(0) {|start, l| start + l.lift_total}
+  end
 end
